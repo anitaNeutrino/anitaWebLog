1ec84308ed2daee5bee2af662325511eb3d3b56b
strutt <strutt@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2015-02-16 16:40:06 +0000

    Added search interface for comments and writers of comments. Made prettier.

--- anitaLog/queryPage.php
+++ anitaLog/queryPage.php
@@ -1,270 +1,286 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
+<html>
+    <head>
+	<meta charset="UTF-8">
+	<title>ANITA-3 Calibration Run Log</title>
+	<link rel="stylesheet" type="text/css" href="logStyle.css">
+    </head>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
+    <div id="header_container">
+	<div id="header">
+	    <header>
+		<h1>ANITA-3 Calibration Run Log</h1>
+		<form action="queryPage.php" method="post">
+		    <p>Enter search information (leave all fields blank to select all).
+		    </p>
+		    <table cellpadding=0 cellspacing=0 border=0 width="600" align center>
+			<tr>
+			    <td rowspan="3">
+				<table cellpadding=0 cellspacing=0 border=0 width="200" align left>
 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="generator" />
-<body bgColor=#BBDDFF><font face="Arial">
+				    <tr>
+					<td>First run</td>
+					<td><input name =firstRun size=10 maxlength=10 value="<?php echo $_POST["firstRun"]; ?>"></td>
+				    </tr>
+				    <tr>
+					<td>Last run</td>
+					<td><input name =lastRun size=10 maxlength=10 value="<?php echo $_POST["lastRun"]; ?>"></td>
+				    </tr>
+				</table>
+			    </td>
 
-<title>ANITA-3 Run Database</title>
+			    <td rowspan="3">
+				<table cellpadding=0 cellspacing=0 border=0 width="200" align left>
+				    <tr>
+					<td>Run Description</td>
+				    </tr>
+				    <tr>
+					<td><input name =runDesc size=22 value="<?php echo $_POST["runDesc"];?>"></td>
+				    </tr>
+				</table>
+			    </td>
 
-</head>
-<body>
+			    <td rowspan="3">
+				<table cellpadding=0 cellspacing=0 border=0 width="200" align left>
+				    <tr>
+					<td>Shifter Name</td>
+				    </tr>
+				    <tr>
+					<td><input name =theShifter size=22 value="<?php echo $_POST["theShifter"];?>"></td>
+				    </tr>
+				</table>
+			    </td>
+			    <td rowspan="3">
+				<table cellpadding=0 cellspacing=0 border=0 width="200" align left>
+				    <tr>
+					<td>Comments By</td>
+				    </tr>
+				    <tr>
+					<td><input name =commentsBy size=22 value="<?php echo $_POST["commentsBy"];?>"></td>
+				    </tr>
+				</table>
+			    </td>
+			    <td rowspan="3">
+				<table cellpadding=0 cellspacing=0 border=0 width="200" align left>
+				    <tr>
+					<td>Comments</td>
+				    </tr>
+				    <tr>
+					<td><input name =comments size=22 value="<?php echo $_POST["comments"];?>"></td>
+				    </tr>
+				</table>
+			    </td>
 
-<h2>ANITA-3 Calibration/Testing Run Database</h2>
-<p>Search the ANITA-3 run database on this page.</p> 
+			    <td rowspan="3">
+				<table cellpadding=0 cellspacing=0 border=0 width="200" allign left>
+				    <tr>
+					<td>Location</td>
+				    </tr>
+				    <tr>
+					<td>
+					    <select name="location">
+						<option value="All" <?php if($_POST["location"]=="All") echo 'selected="selected"'; ?>>All</option>
+						<option value="Antarctica" <?php if($_POST["location"]=="Antarctica") echo 'selected="selected"'; ?>>Antarctica</option> 
+						<option value="Palestine" <?php if($_POST["location"]=="Palestine") echo 'selected="selected"'; ?>>Palestine/Hawaii</option>
+					    </select>
+					</td>
+				    </tr>
+				</table>
+			    </td>
 
-<p>If you wish to add comments about specific runs, enter the information <a href="logForm.html">here</a>.</p>
+			    <td rowspan="3">
+				<table cellpadding=0 cellspacing=0 border=0 width="200" allign left>
+				    <tr>
+					<td>Order</td>
+				    </tr>
+				    <tr>
+					<td>
+					    <select name="order">
+						<option value="descending" <?php if($_POST["order"]=="descending") echo 'selected="selected"'; ?>>Descending</option> 
+						<option value="ascending"<?php if($_POST["order"]=="ascending") echo 'selected="selected"'; ?>>Ascending</option>
+					    </select>
+					</td>
 
-<hr />
+				    </tr>
+				</table>
+			    </td>
+		    </table>
+		    <br/>
+		    <input type="submit" value="Search">
+		</form>
+	    </header>
+	</div>
+    </div>
 
-<form action="queryPage.php" method="post">
+    <body>
+	<?php
+	error_reporting(E_ALL);
+	$firstRun = $_POST["firstRun"];
+	$lastRun = $_POST["lastRun"];
+	$runDescSearch = $_POST["runDesc"];
+	$commentsBySearch = $_POST["commentsBy"];
+	$commentsSearch = $_POST["comments"];
+	$theShifter = $_POST["theShifter"];
 
-  <p>Which runs would you like information about (leave all fields blank to select all)?<br>
-  </p>
+	$order = $_POST["order"];
+	$aloc = $_POST["location"];
 
-  <table cellpadding=0 bgcolor=#BBDDFF cellspacing=0 border=0 width="600" allign center>
-  <tr>
+	$dbhost = "localhost";
+	$dbuser = "anita";
+	$dbpass = "IceRadi0";
+	$dbName = "runLog";
 
-    <td rowspan="3">
-      <table cellpadding=0 cellspacing=0 border=0 width="200" allign left>
+	$selRunNumber = " ";
+	$location = " ";
+	$startTime = " ";
+	$endTime = " ";
+	$firstEvent = " ";
+	$lastEvent = " ";
+	$shifterName = " ";
+	$runDescription = " ";
+	$commentName = " ";
+	$comment = " ";
 
-        <tr>
-	  <td>First run</td>
-	  <td><input name =firstRun size=10 maxlength=10 value="<?php echo $_POST["firstRun"]; ?>"></td>
-	</tr>
-	<tr>
-	  <td>Last run</td>
-	  <td><input name =lastRun size=10 maxlength=10 value="<?php echo $_POST["lastRun"]; ?>"></td>
-	</tr>
-      </table>
-    </td>
+	// Make connection to sql database
+	$link = mysql_connect('localhost', 'anita', 'IceRadi0') or die('Error! Could not connect to server.');
+	$db_selected = mysql_select_db('runLog', $link) or die('Error! Could not find database.');
 
-    <td rowspan="3">
-      <table cellpadding=0 cellspacing=0 border=0 width="200" allign left>
-        <tr>
-	  <td>Search run comments</td>
-	</tr>
-	<tr>
-	  <td><input name =textSearch size=22 value="<?php echo $_POST["textSearch"];?>"></td>
-	</tr>
-      </table>
-    </td>
+	if(!$lastRun){
+	    $lastRun = 9999999999; # For now...
+	}
+	if(!$firstRun){
+	    $firstRun = 1;
+	}
+	if($firstRun > $lastRun){
+	    die('Error! Make sure first run is earlier than last run.');
+	}
+	if($theShifter){
+	    $findShifter=$theShifter;
+	}
 
-    <td rowspan="3">
-      <table cellpadding=0 cellspacing=0 border=0 width="200" allign left>
-        <tr>
-	  <td>Shifter Name</td>
-	</tr>
-	<tr>
-	  <td><input name =theShifter size=22 value="<?php echo $_POST["theShifter"];?>"></td>
-	</tr>
-      </table>
-    </td>
+	$runsFound=0;
+	$startRun=0;
+	$endRun=0;
+	$iterater=0;
+	if($order=="ascending"){
+	    $orderCommand = "ASC";
+	    $startRun=$firstRun;
+	    $endRun=$lastRun;
+	    $iterator=1;
+	}
+	else{
+	    $orderCommand = "DESC";
+	    $startRun=$lastRun;
+	    $endRun=$firstRun;
+	    $iterator=-1;
+	}
 
-  </tr>
-  </table>
+	$result = mysql_query("SELECT * FROM runTable WHERE run>=$firstRun AND run<=$lastRun ORDER BY run $orderCommand");
+	while($row = mysql_fetch_assoc($result, MYSQL_NUM)){
+	    $runNumber = $row[0];
+	    $location = $row[1];
+	    $startTime = $row[2];
+	    $endTime = $row[3];
+	    $firstEvent = $row[4];
+	    $lastEvent = $row[5];
+	    $shifterName = $row[6];
+	    $runDescription = $row[7];
+	    $commentName = $row[8];
+	    $comment = $row[9];
 
-<p>Select location of run?<br /></p>
-<input type="checkbox" name="location[]" value="Antarctica" checked="checked"/>Antarctica<br />
-<input type="checkbox" name="location[]" value="Palestine" />Palestine/Hawaii<br /> 
+	    # By default there's a match
+	    $foundComment=1;
 
-<p>Run order?<br /></p>
-<input type="radio" name="order" value="Ascending" />Ascending<br /> 
-<input type="radio" name="order" value="Descending" checked="checked"/>Descending<br />
+	    #If we have search terms, then allow rows to not be matched...
 
-<input type="submit" value="Get Info">
-</form>
-
-
-<?php
-error_reporting(E_ALL);
-$firstRun = $_POST["firstRun"];
-$lastRun = $_POST["lastRun"];
-$textSearch = $_POST["textSearch"];
-$theShifter = $_POST["theShifter"];
-
-$order = $_POST["order"];
-$aloc = array($_POST["location"]);
-$antLoc = 0;
-if(array_key_exists(0, $aloc)){
-    $antLoc = $aloc[0];
-}
-$palLoc = 0;
-if(array_key_exists(1, $aloc)){
-    $palLoc = $aloc[1];
-}
-
-$textArray = explode(" ",$textSearch);
-$textArraySize = count($textArray);
-
-$dbhost = "localhost";
-$dbuser = "anita";
-$dbpass = "IceRadi0";
-$dbName = "runLog";
-
-$selRunNumber = " ";
-$location = " ";
-$startTime = " ";
-$endTime = " ";
-$firstEvent = " ";
-$lastEvent = " ";
-$shifterName = " ";
-$runDescription = " ";
-$logName = " ";
-$logComment = " ";
-
-if(!$firstRun && !$lastRun){
-    $firstRun=1;
-    $lastRun=19999;
-}
-
-if(!$lastRun){
-    //  $lastRun = $firstRun;
-    $lastRun = 19999;
-}
-if(!$firstRun){
-    //  $firstRun = $lastRun;
-    $firstRun = 0;
-}
-if($firstRun > $lastRun){
-    die('Make sure first run is earlier than last run!');
-}
-if($theShifter){
-    $findShifter=$theShifter;
-}
-
-$link = mysql_connect('localhost', 'anita', 'IceRadi0') or die('Error! Could not connect to server');
-$db_selected = mysql_select_db('runLog', $link) or die('Error! Could not find database');
-$runsFound=0;
-$startRun=0;
-$endRun=0;
-$iterater=0;
-if($order=="Ascending"){
-    $startRun=$firstRun;
-    $endRun=$lastRun;
-    $iterator=1;
-}
-else{
-    $startRun=$lastRun;
-    $endRun=$firstRun;
-    $iterator=-1;
-}
-
-$runNumber = $startRun-$iterator;
-while($runNumber!=$endRun){
-    $runNumber += $iterator;
-    $result = mysql_query("SELECT * FROM runTable WHERE run=".$runNumber);
-    if($result != FALSE){
-        $row = mysql_fetch_array($result, MYSQL_NUM);
-        $location = $row[1];
-        $startTime = $row[2];
-        $endTime = $row[3];
-        $firstEvent = $row[4];
-        $lastEvent = $row[5];
-        $shifterName = $row[6];
-        $runDescription = $row[7];
-        $logName = $row[8];
-        $logComment = $row[9];
-
-        // Require location matching
-        if($location==$antLoc){
-        }
-        else if($location==$palLoc){
-        }
-        else{
-            continue;
-        }
-
-        $foundComment=1;
-	
-        if($textSearch){
-
-            for($textElement=0;$textElement<$textArraySize;$textElement++){
-		$foundCommentArray[$textElement]=0;
+	    # Shifter name match?
+	    if($theShifter){
+		if( !((strpos($shifterName, $theShifter) !== FALSE)) ){
+		    $foundComment=0;
+		}
 	    }
 
-            $descriptionArray = explode(" ",$runDescription);
-            $commentArray = explode(" ",$logComment);
-
-            $arraySize = count($descriptionArray);
-            for($arrayElement=0;$arrayElement<$arraySize;$arrayElement++){
-		for($textElement=0;$textElement<$textArraySize;$textElement++){
-		    if(strtolower($textArray[$textElement])==strtolower($descriptionArray[$arrayElement])){
-        		$foundCommentArray[$textElement]=1;
-		    }
+	    # Run description match?
+	    if($runDescSearch){
+		if( !((strpos($runDescription, $runDescSearch) !== FALSE)) ){
+		    $foundComment=0;
 		}
-            }
+	    }
 
-            $arraySize = count($commentArray);
-            for($arrayElement=0;$arrayElement<$arraySize;$arrayElement++){
-		for($textElement=0;$textElement<$textArraySize;$textElement++){
-		    if(strtolower($textArray[$textElement])==strtolower($descriptionArray[$arrayElement])){
-         		$foundCommentArray[$textElement]=1;
-		    }
+	    # Comments by match?
+	    if($commentsBySearch){
+		if( !((strpos($commentName, $commentsBySearch) !== FALSE)) ){
+		    $foundComment=0;
 		}
-            }
+	    }
 
-	    
-            for($textElement=0;$textElement<$textArraySize;$textElement++){
-		if($foundCommentArray[$textElement]==0){
-         	    $foundComment=0;
+	    # Comments by match?
+	    if($commentsSearch){
+		if( !((strpos($comment, $commentsSearch) !== FALSE)) ){
+		    $foundComment=0;
 		}
-            }
+	    }
 
 
-	} // if($textSearch)
+	    # Location match?
+	    if(!($location==$aloc || $aloc == "All")){
+		$foundComment=0;
+	    }
 
-	if(!$theShifter){
-            $findShifter=$shifterName;
-	}
-	
-	if(!$row){
-	}
-	else if($foundComment==1 && strtolower($findShifter)==strtolower($shifterName)){
-            $runsFound++;
-	    if($runsFound==1){
-		echo "<hr />";
+	    # Run description match?
+	    if($runDescSearch){
+		if( !((strpos($runDescription, $runDescSearch) !== FALSE)) ){
+		    $foundComment=0;
+		}
+	    }
 
-		echo "<table cellpadding=10 cellspacing=1 border=1 width=100%>
+
+	    if(!$row){
+	    }
+	    else if($foundComment==1){
+		$runsFound++;
+
+		# Print header row of table if this is the first entry
+		if($runsFound==1){
+		    echo "<hr />";
+
+		    echo "<table cellpadding=10 cellspacing=1 border=1 width=100%>
   <tr>
     <td width=3%>Run</td>
     <td width=5%>Loc.</td>
     <td width=12%>Start Time</td>
     <td width=12%>End Time</td>
+    <td width=4%>Shifter</td>
+    <td width=15%>Run Description</td>
+    <td width=5%>Comments By</td>
+    <td width=15%>Comments</td>
     <td width=6%>First Event</td>
     <td width=6%>Last Event</td>
-    <td width=4%>Shifter</td>
-    <td width=15%>Shift Details</td>
-    <td width=5%>Log Name</td>
-    <td width=15%>Log Entries</td>
   </tr>";
-	    }
+		}
 
-	    echo "
+		echo "
   <tr>
     <td width=3%>$runNumber</td>
     <td width=3%> $location</td>
     <td width=12%> $startTime</td>
     <td width=12%> $endTime</td>
-    <td width=6%> $firstEvent</td>
-    <td width=6%> $lastEvent</td>
     <td width=4%> $shifterName</td>
     <td width=15%> $runDescription</td>
-    <td width=5%> $logName</td>
-    <td width=15%> $logComment</td>
+    <td width=5%> $commentName</td>
+    <td width=15%> $comment</td>
+    <td width=6%> $firstEvent</td>
+    <td width=6%> $lastEvent</td>
+
   </tr>";
+	    }
 	}
-    }
-}
+	if($runsFound==0){
+	    echo "<p>Sorry. No runs found matching your search criteria.<p/>";
+	}
 
-if($runsFound==0){
-    echo "<p>Sorry. No runs found matching your search criteria.<p/>";
-}
+	echo "</table>";
+	?>
 
-echo "</table>";
-?>
-
-</body>
+    </body>
 </html>
